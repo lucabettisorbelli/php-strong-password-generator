4ce4c8cec11364edcb2d127b49dae33794563405
luca <Luca betti sorbelli>
2023-06-09 15:23:13 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-strong-password-generator</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="titolo text-center py-2">
+                    <h1>Genera una password sicura</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
